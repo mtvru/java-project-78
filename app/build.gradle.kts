984c64e99ca1f3f0144297585a5ca9a8fe6059b8
maksimus <max.expertdev@gmail.com>
2026-01-26 20:34:43 +0600

    chore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,9 +36,6 @@ jacoco {
 }
 
 dependencies {
-    implementation("info.picocli:picocli:4.7.7")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.1")
-    annotationProcessor("info.picocli:picocli-codegen:4.7.7")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
